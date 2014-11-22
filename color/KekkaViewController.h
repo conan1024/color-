50deaf0b0620dcce5fcbef5d2ec4f62cb1c554da
MIYAMOTOMASANARI <generalconan1024@gmail.com>
2014-11-22 17:59:34 +0900

    タイマー完成

---
+++ color/KekkaViewController.h
@@ -0,0 +1,13 @@
+//
+//  KekkaViewController.h
+//  color
+//
+//  Created by Masanari Miyamoto on 2014/11/22.
+//  Copyright (c) 2014年 Masanari Miyamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KekkaViewController : UIViewController
+
+@end
---
+++ color/KekkaViewController.m
@@ -0,0 +1,37 @@
+//
+//  KekkaViewController.m
+//  color
+//
+//  Created by Masanari Miyamoto on 2014/11/22.
+//  Copyright (c) 2014年 Masanari Miyamoto. All rights reserved.
+//
+
+#import "KekkaViewController.h"
+
+@interface KekkaViewController ()
+
+@end
+
+@implementation KekkaViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
